8ce9e1862f5d7e84699cd0e892d81ba265444667
Arvind Satyanarayan <arvindsatya1@gmail.com>
2018-09-07 13:29:39 -0400

    Add Amy and Jonathan.

--- src/data/people.js
+++ src/data/people.js
@@ -1,134 +1,143 @@
 module.exports = {
   arvindsatya: {
-    name: 'Arvind Satyanarayan',
-    url: 'http://arvindsatya.com',
-    title: 'Assistant Professor'
+    name: "Arvind Satyanarayan",
+    url: "http://arvindsatya.com",
+    title: "Assistant Professor"
+  },
+  amyshea: {
+    name: "Amy Shea-Slattery",
+    title: "Administrative Assistant"
   },
   aspenhopkins: {
-    name: 'Aspen Hopkins',
-    title: 'PhD Student'
+    name: "Aspen Hopkins",
+    title: "PhD Student"
   },
   alundgard: {
-    name: 'Alan Lundgard',
-    url: 'http://alundgard.com',
-    title: 'PhD Student'
+    name: "Alan Lundgard",
+    url: "http://alundgard.com",
+    title: "PhD Student"
+  },
+  jzong: {
+    name: "Jonathan Zong",
+    url: "http://jonathanzong.com/",
+    title: "PhD Student"
   },
   jheer: {
-    name: 'Jeffrey Heer',
-    url: 'http://jheer.org',
-    role: 'external'
+    name: "Jeffrey Heer",
+    url: "http://jheer.org",
+    role: "external"
   },
   ryanrussell: {
-    name: 'Ryan Russell',
-    url: 'https://github.com/RussellSprouts',
-    role: 'external'
+    name: "Ryan Russell",
+    url: "https://github.com/RussellSprouts",
+    role: "external"
   },
   jhoffs: {
-    name: 'Jane Hoffswell',
-    url: 'https://homes.cs.washington.edu/~jhoffs/',
-    role: 'external'
+    name: "Jane Hoffswell",
+    url: "https://homes.cs.washington.edu/~jhoffs/",
+    role: "external"
   },
   kanitw: {
-    name: 'Kanit Wongsuphasawat',
-    url: 'http://kanitw.github.io/',
-    role: 'external'
+    name: "Kanit Wongsuphasawat",
+    url: "http://kanitw.github.io/",
+    role: "external"
   },
   domoritz: {
-    name: 'Dominik Moritz',
-    url: 'http://domoritz.de',
-    role: 'external'
+    name: "Dominik Moritz",
+    url: "http://domoritz.de",
+    role: "external"
   },
   oranju: {
-    name: 'Ranjitha Kumar',
-    url: 'http://ranjithakumar.net/',
-    role: 'external'
+    name: "Ranjitha Kumar",
+    url: "http://ranjithakumar.net/",
+    role: "external"
   },
   jtalton: {
-    name: 'Jerry Talton',
-    fullName: 'Jerry O. Talton',
-    url: 'http://jerrytalton.net',
-    role: 'external'
+    name: "Jerry Talton",
+    fullName: "Jerry O. Talton",
+    url: "http://jerrytalton.net",
+    role: "external"
   },
   maxinelim: {
-    name: 'Maxine Lim',
-    url: 'http://maxinelim.com/',
-    role: 'external'
+    name: "Maxine Lim",
+    url: "http://maxinelim.com/",
+    role: "external"
   },
   cearto: {
-    name: 'Cesar Torres',
-    url: 'http://cearto.com/',
-    role: 'external'
+    name: "Cesar Torres",
+    url: "http://cearto.com/",
+    role: "external"
   },
   saahmad: {
-    name: 'Salman Ahmad',
-    url: 'http://salmanahmad.com/',
-    role: 'external'
+    name: "Salman Ahmad",
+    url: "http://salmanahmad.com/",
+    role: "external"
   },
   srk: {
-    name: 'Scott Klemmer',
-    fullName: 'Scott R. Klemmer',
-    url: 'http://d.ucsd.edu/srk/',
-    role: 'external'
+    name: "Scott Klemmer",
+    fullName: "Scott R. Klemmer",
+    url: "http://d.ucsd.edu/srk/",
+    role: "external"
   },
   weibel: {
-    name: 'Nadir Weibel',
-    url: 'http://weibel.ucsd.edu/',
-    role: 'external'
+    name: "Nadir Weibel",
+    url: "http://weibel.ucsd.edu/",
+    role: "external"
   },
   hollan: {
-    name: 'Jim Hollan',
-    fullName: 'James D. Hollan',
-    url: 'http://hci.ucsd.edu/hollan/',
-    role: 'external'
+    name: "Jim Hollan",
+    fullName: "James D. Hollan",
+    url: "http://hci.ucsd.edu/hollan/",
+    role: "external"
   },
   strazzulla: {
-    name: 'Daniel Strazzulla',
-    url: 'http://strazzulla.me/',
-    role: 'external'
+    name: "Daniel Strazzulla",
+    url: "http://strazzulla.me/",
+    role: "external"
   },
   clemens: {
-    name: 'Clemens Klokmose',
-    url: 'http://www.klokmose.net/',
-    role: 'external'
+    name: "Clemens Klokmose",
+    url: "http://www.klokmose.net/",
+    role: "external"
   },
   mbl: {
-    name: 'Michel Beaudouin-Lafon',
-    url: 'https://www.lri.fr/~mbl/',
-    role: 'external'
+    name: "Michel Beaudouin-Lafon",
+    url: "https://www.lri.fr/~mbl/",
+    role: "external"
   },
   mackay: {
-    name: 'Wendy Mackay',
-    url: 'https://www.lri.fr/~mackay/',
-    role: 'external'
+    name: "Wendy Mackay",
+    url: "https://www.lri.fr/~mackay/",
+    role: "external"
   },
   colah: {
-    name: 'Chris Olah',
-    url: 'https://colah.github.io',
-    role: 'external'
+    name: "Chris Olah",
+    url: "https://colah.github.io",
+    role: "external"
   },
   enjalot: {
-    name: 'Ian Johnson',
-    url: 'https://github.com/enjalot',
-    role: 'external'
+    name: "Ian Johnson",
+    url: "https://github.com/enjalot",
+    role: "external"
   },
   shancarter: {
-    name: 'Shan Carter',
-    url: 'http://shancarter.com/',
-    role: 'external'
+    name: "Shan Carter",
+    url: "http://shancarter.com/",
+    role: "external"
   },
   ludwigschubert: {
-    name: 'Ludwig Schubert',
-    url: 'https://schubert.io/',
-    role: 'external'
+    name: "Ludwig Schubert",
+    url: "https://schubert.io/",
+    role: "external"
   },
   kqy: {
-    name: 'Katherine Ye',
-    url: 'https://cs.cmu.edu/~kqy/',
-    role: 'external'
+    name: "Katherine Ye",
+    url: "https://cs.cmu.edu/~kqy/",
+    role: "external"
   },
   znah: {
-    name: 'Alexander Mordvintsev',
-    url: 'https://znah.net/',
-    role: 'external'
+    name: "Alexander Mordvintsev",
+    url: "https://znah.net/",
+    role: "external"
   }
 };
