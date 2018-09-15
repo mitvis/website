bfe9787a9f803c90c999c220dfea3e323cb20373
Admin <arlu@umich.edu>
2018-09-15 12:40:58 -0400

    Added routes and mission partial

--- scripts/build.js
+++ scripts/build.js
@@ -12,20 +12,24 @@ var reload = require('require-reload')(require),
 
 function build() {
   try {
+    // Register partials
     fs.readdirSync('src/html').forEach(function(filename) {
       var basename = path.basename(filename, '.html');
       hb.registerPartial(basename,
         fs.readFileSync('src/html/' + filename, {encoding: 'utf-8'}));
     });
 
-    var data = reload('../src/data'),
-        html = fs.readFileSync('src/index.html', {encoding: 'utf-8'}),
+    // Write routes
+    fs.readdirSync('src/routes').forEach(function(filename) {
+      var data = reload('../src/data'),
+        html = fs.readFileSync('src/routes/'+filename, {encoding: 'utf-8'}),
         scss = fs.readFileSync('src/scss/index.scss', {encoding: 'utf-8'}),
         tmpl = hb.compile(html),
-        css  = sass.renderSync({data: scss});
+        css = sass.renderSync({data: scss});
+        fs.writeFileSync('build/'+filename, tmpl(data));
+        fs.writeFileSync('build/styles.css', css.css);
+    });
 
-    fs.writeFileSync('build/index.html', tmpl(data));
-    fs.writeFileSync('build/styles.css', css.css);
   } catch (e) {
     console.error(timeF(new Date), chalk.red(e), e);
   }
