c91d9b67215e9211b8c942713b98314ce0ae7177
Admin <arlu@umich.edu>
2019-02-09 21:27:25 -0500

    Remove hidden:true from uist 2014

--- src/data/papers.js
+++ src/data/papers.js
@@ -52,7 +52,7 @@ module.exports = [
     authors: ['arvindsatya', 'kanitw', 'jheer'],
     venue: 'uist',
     year: 2014,
-    hidden: true,
+    // hidden: true,
     url: '/pdf/2014-DeclarativeInteraction-UIST.pdf',
     abstract: 'Declarative visualization grammars can accelerate development, facilitate retargeting across platforms, and allow language-level optimizations. However, existing declarative visualization languages are primarily concerned with visual encoding, and rely on imperative event handlers for interactive behaviors. In response, we introduce a model of declarative interaction design for data visualizations. Adopting methods from reactive programming, we model low-level events as composable data <em>streams</em> from which we form higher-level semantic <em>signals</em>. Signals feed <em>predicates</em> and <em>scale inversions</em>, which allow us to generalize interactive selections at the level of item geometry (pixels) into interactive queries over the data domain. <em>Production rules</em> then use these queries to manipulate the visualization’s appearance. To facilitate reuse and sharing, these constructs can be encapsulated as named <em>interactors</em>: standalone, purely declarative specifications of interaction techniques. We assess our model’s feasibility and expressivity by instantiating it with extensions to the Vega visualization grammar. Through a diverse range of examples, we demonstrate coverage over an established taxonomy of visualization interaction techniques.',
     featured: false
