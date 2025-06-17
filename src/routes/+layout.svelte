aae16ff6adb00c330cabfba387ce1fc54f817ace
Arvind Satyanarayan <arvindsatya@mit.edu>
2025-06-17 10:25:33 -0400

    Convert _data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import '../app.css';
 
 	let { children } = $props();
+
+	export const prerender = true;
 </script>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+  import type { PageProps } from './$types';
+
+  let { data }: PageProps = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#each data.news as item}
+  <div class="item">
+    <p class="date">{item.date}</p>
+    {@html item.desc}
+  </div>
+{/each}
