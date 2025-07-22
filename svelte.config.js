4670d4e4aff2d7c671f861e401b8cbb3a76307d9
Arvind Satyanarayan <arvindsatya@mit.edu>
2025-07-22 14:07:39 -0400

    refactor: use adapter-static for SSG

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
-	kit: { adapter: adapter() },
+	kit: { 
+		adapter: adapter(), 
+		prerender: { handleHttpError: 'warn', handleMissingId: 'warn' } 
+	},
 	extensions: ['.svelte', '.svx', '.md']
 };
 
