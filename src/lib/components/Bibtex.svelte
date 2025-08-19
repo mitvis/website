fe67a1672ce00f7d8b4c5bc3028668c91147558c
Arvind Satyanarayan <arvindsatya@mit.edu>
2025-08-18 17:56:02 -0700

    fix: Bibtex should show paper's fullTitle

--- src/lib/components/Bibtex.svelte
+++ src/lib/components/Bibtex.svelte
@@ -40,7 +40,7 @@
 
 <div class="text-xs bg-stone-100 rounded-md p-2 mb-4 overflow-x-auto">
   <pre bind:this={bibtex}>@{venue.bibtex.type}&lbrace;{displayYear}-{slug}
-  title = &lbrace;&lbrace;{escape(pub.title)}&rbrace;&rbrace;,
+  title = &lbrace;&lbrace;{escape(pub.fullTitle)}&rbrace;&rbrace;,
   author = &lbrace;{escape(authors)}&rbrace;,
   {venue.bibtex.venue} = &lbrace;{escape(venue.full)}&rbrace;,
   year = &lbrace;{displayYear}&rbrace;,
