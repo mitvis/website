961b0f3c77e2af7301be2b0dd70ec65b046b1e0c
arvind <arvind@users.noreply.github.com>
2025-12-10 20:00:53 +0000

    Build: (270ee39) Bump devalue from 5.1.1 to 5.3.2 (#91)
    Bumps [devalue](https://github.com/sveltejs/devalue) from 5.1.1 to 5.3.2.
    - [Release notes](https://github.com/sveltejs/devalue/releases)
    - [Changelog](https://github.com/sveltejs/devalue/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/sveltejs/devalue/compare/v5.1.1...v5.3.2)
    
    ---
    updated-dependencies:
    - dependency-name: devalue
      dependency-version: 5.3.2
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ _app/immutable/entry/start.BWeJRKNo.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Yl6h41uQ.js";export{o as load_css,r as start};
--- _app/immutable/entry/start.DpHoTUFV.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/iwn4rJb5.js";export{o as load_css,r as start};
