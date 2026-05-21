8d2614ad9a7d1ae8899197da6f77e80c638ed889
arvind <arvind@users.noreply.github.com>
2026-05-21 16:57:59 +0000

    Build: (e64973e) Fix browser history bug by using Sveltekit shallow routing

---
+++ _app/immutable/chunks/CRdMYAEX.js
@@ -0,0 +1,1 @@
+import{s as r,p as t}from"./BJ5CjjXm.js";const e={get data(){return t.data},get error(){return t.error},get params(){return t.params},get state(){return t.state},get status(){return t.status},get url(){return t.url}};r.updated.check;const s=e;export{s as p};
--- _app/immutable/nodes/2.CyYUwkgw.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/CfdRNuc5.js";export{e as component,n as universal};
