98dd4b991a5f77328754526aea9f5ccd8a1a9bb3
arvind <arvind@users.noreply.github.com>
2026-09-15 11:35:50 +0000

    Build: (94f844d) Bump @vitest/mocker and vitest (#126)
    Bumps [@vitest/mocker](https://github.com/vitest-dev/vitest/tree/HEAD/packages/mocker) to 5.0.1 and updates ancestor dependency [vitest](https://github.com/vitest-dev/vitest/tree/HEAD/packages/vitest). These dependencies need to be updated together.
    
    
    Updates `@vitest/mocker` from 3.2.3 to 5.0.1
    - [Release notes](https://github.com/vitest-dev/vitest/releases)
    - [Changelog](https://github.com/vitest-dev/vitest/blob/main/docs/releases.md)
    - [Commits](https://github.com/vitest-dev/vitest/commits/v5.0.1/packages/mocker)
    
    Updates `vitest` from 3.2.3 to 5.0.1
    - [Release notes](https://github.com/vitest-dev/vitest/releases)
    - [Changelog](https://github.com/vitest-dev/vitest/blob/main/docs/releases.md)
    - [Commits](https://github.com/vitest-dev/vitest/commits/v5.0.1/packages/vitest)
    
    ---
    updated-dependencies:
    - dependency-name: "@vitest/mocker"
      dependency-version: 5.0.1
      dependency-type: indirect
    - dependency-name: vitest
      dependency-version: 5.0.1
      dependency-type: direct:development
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- _app/immutable/nodes/2.Cx6MADFE.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/D6JG6Asy.js";export{e as component,n as universal};
---
+++ _app/immutable/nodes/2.YKCy-S6Q.js
@@ -0,0 +1,1 @@
+import{a as e,_ as n}from"../chunks/BU0bU6O0.js";export{e as component,n as universal};
